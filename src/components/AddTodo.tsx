e0b0ca426ffe379557be2aac1b30d3f47ea1401a
blintdm98@gmail.com <blintdm98@gmail.com>
2024-06-27 13:18:44 +0300

    add useForm for project which help register todo text

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import { Box, Button, TextField } from '@mui/material';
+import { useForm } from 'react-hook-form';
+
+type AddState = {
+    todo: string
+}
 
 function AddTodo() {
-  return (
-    <Box sx={{ display: 'flex', width:'80%', mb: 2 }}>
-      <TextField fullWidth placeholder="Add Todo" />
-      <Button variant="contained" color="primary" sx={{ ml: 2 }}>
-        Add
-      </Button>
-    </Box>
-  );
-}
+    const { register, handleSubmit} = useForm<AddState>();
+    const onSubmit = (data: AddState) => {console.log(data)};
+    return (
+        <Box sx={{ display: 'flex', width:'80%', mb: 2 }}
+        >
+            <form onSubmit={handleSubmit(onSubmit)} style={{display: 'flex', width: '100%'}}>
+            <TextField fullWidth placeholder="Add Todo" {...register("todo", {max: 50})} />
+            <Button variant="contained" color="primary" sx={{ ml: 2 }} type='submit'>
+                Add
+            </Button>
+            </form>
+        </Box>
+    );
+    }
 
 export default AddTodo;
